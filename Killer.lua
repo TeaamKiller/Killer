b07659fe7e335e356e952b0ab1f61e003b695115
TeaamKiller <82029196+TeaamKiller@users.noreply.github.com>
2021-04-06 14:34:54 +0300

    Add files via upload

--- Killer.lua
+++ Killer.lua
@@ -4776,8 +4776,8 @@ database:set(bot_id.."Killer:Left:Bot"..msg.chat_id_,true)
 send(msg.chat_id_, msg.id_, "📫┇تم تعطيل مغادرة البوت") 
 return false 
 end
-if text == (database:get(bot_id.."Killer:Name:Bot") or "تشاكيx") then
-Namebot = (database:get(bot_id.."Killer:Name:Bot") or "تشاكيx")
+if text == (database:get(bot_id.."Killer:Name:Bot") or "KILLER") then
+Namebot = (database:get(bot_id.."Killer:Name:Bot") or "KILLER")
 local namebot = {
 "عمري فداك "..Namebot.. " كول حب ",
 "كول حبيبي ؟ اني "..Namebot,
@@ -4796,7 +4796,7 @@ return false
 end
 
 if text == "بوت" then
-Namebot = (database:get(bot_id.."Killer:Name:Bot") or "تشاكيx")
+Namebot = (database:get(bot_id.."Killer:Name:Bot") or "KILLER")
 send(msg.chat_id_, msg.id_," ["..Namebot.."] ") 
 end
 if text == "تغير اسم البوت" or text == "تغيير اسم البوت" then 
@@ -6161,7 +6161,7 @@ if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
 if Get_info then
-local TextS = "\n📂┇اهلا بك في متجر ملفات تشاكيx \n📮┇الملفات الموجوده حاليا \nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ\n\n"
+local TextS = "\n📂┇اهلا بك في متجر ملفات KILLER \n📮┇الملفات الموجوده حاليا \nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ\n\n"
 local TextE = "\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ\n📌┇تدل علامة (✔) الملف مفعل\n".."📌┇تدل علامة (✖) الملف معطل\n"
 local NumFile = 0
 for name,Info in pairs(res.plugins_) do
@@ -8743,7 +8743,7 @@ if NewCmmd then
 data.message_.content_.text_ = (NewCmmd or data.message_.content_.text_)
 end
 end
-local Name_Bot = (database:get(bot_id.."Killer:Name:Bot") or "تشاكيx")
+local Name_Bot = (database:get(bot_id.."Killer:Name:Bot") or "KILLER")
 if not database:get(bot_id.."Killer:Fun_Bots"..msg.chat_id_) then
 if text ==  ""..Name_Bot..' شنو رئيك بهاذا' and tonumber(msg.reply_to_message_id_) > 0 then     
 function FunBot(extra, result, success) 
